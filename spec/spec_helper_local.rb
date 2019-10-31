a82fdf060f024e13e73477742a3c685defeb427e
mergwyn <gary@theclarkhome.com>
2019-10-31 09:39:49 +0000

    Add coveralls

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -1,4 +1,19 @@
+require 'simplecov'
 require_relative './helpers'
+
+if ENV['SIMPLECOV']
+  SimpleCov.formatters = [
+    SimpleCov::Formatter::HTMLFormatter,
+  ]
+  SimpleCov.start { add_filter '/spec/' }
+elsif ENV['TRAVIS'] && RUBY_VERSION.to_f >= 1.9
+  require 'coveralls'
+  SimpleCov.formatters = [
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter,
+  ]
+  Coveralls.wear! { add_filter '/spec/' }
+end
 
 RSpec.configure do |c|
   c.include Helpers
