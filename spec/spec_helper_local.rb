6870b3f1ffffeac58784cfd81b47045b8e672b2b
mergwyn <gary@theclarkhome.com>
2019-10-31 10:12:05 +0000

    Add track files for coveralls

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -2,14 +2,21 @@ require 'simplecov'
 
 if ENV['SIMPLECOV']
   SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
+  SimpleCov.start { add_filter '/spec/' }
 elsif ENV['TRAVIS'] && RUBY_VERSION.to_f >= 1.9
   require 'coveralls'
   SimpleCov.formatters = [
     SimpleCov::Formatter::HTMLFormatter,
     Coveralls::SimpleCov::Formatter,
   ]
+  SimpleCov.start do
+    track_files 'lib/**/*.rb'
+    add_filter '/spec'
+    add_filter '/vendor'
+    add_filter '/.vendor'
+  end
+
 end
-SimpleCov.start { add_filter '/spec/' }
 
 require_relative './helpers'
 RSpec.configure do |c|
